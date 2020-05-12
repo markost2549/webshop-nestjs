331b1d2bbab3350adc80655ad8240ca199c7b417
Marko Stojkovic <markost2549@gmail.com>
2020-05-12 13:24:13 +0200

    Init commit

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+
+
+@Controller()
+export class AppController {
+
+  @Get()
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  @Get('world')
+  getWorld(): string {
+    return 'World!';
+  }
+
+
+}
