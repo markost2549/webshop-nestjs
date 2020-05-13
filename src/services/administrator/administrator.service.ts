c7e514ff9f915b318f4688785fd0becb5dba028c
Marko Stojkovic <markost2549@gmail.com>
2020-05-13 16:51:22 +0200

    edit api

--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Administrator } from '../../../entities/administrator.entity';
 import { Repository } from 'typeorm';
 import { AddAdministratorDto } from '../../dtos/administrator/add.administrator.dto';
+import { EditAdministratorDto } from 'src/dtos/administrator/edit.administrator.dto';
 
 
 @Injectable()
@@ -25,7 +26,6 @@ export class AdministratorService {
         const crypto = require('crypto');
         const passwordHash = crypto.createHash('sha512')
         passwordHash.update(data.password)
-
         const passwordHashString = passwordHash.digest('hex').toUpperCase();
 
         const newAdmin: Administrator = new Administrator();
@@ -33,5 +33,21 @@ export class AdministratorService {
         newAdmin.passwordHash = passwordHashString
 
         return this.administrator.save(newAdmin)
+    }
+
+    async editById(id: number, data: EditAdministratorDto) {
+        const admin = await this.administrator.findOne(id);
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const crypto = require('crypto');
+        const passwordHash = crypto.createHash('sha512')
+        passwordHash.update(data.password)
+        const passwordHashString = passwordHash.digest('hex').toUpperCase();
+
+        admin.passwordHash = passwordHashString;
+
+        return this.administrator.save(admin)
+
+
+
     }
 }
