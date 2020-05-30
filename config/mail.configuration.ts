8d39afb0e396cde589ec687355803a11f61ad8b7
Marko Stojkovic <markost2549@gmail.com>
2020-05-30 22:19:15 +0200

    Mail configuration

---
+++ config/mail.configuration.ts
@@ -0,0 +1,8 @@
+export const MailConfig = {
+    hostname: 'smtp.gmail.com',
+    username: 'user',
+    password: 'password',
+    senderEmail: 'user@gmail.com',
+
+    orderNotificationMail: 'user@gmail.com'
+}
