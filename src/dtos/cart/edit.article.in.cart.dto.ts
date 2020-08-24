fa2155a3538d836376bc143328a20e4efbd43769
Marko Stojkovic <markost2549@gmail.com>
2020-08-24 19:12:39 +0200

    Fixed cart validator

--- src/dtos/cart/edit.article.in.cart.dto.ts
+++ src/dtos/cart/edit.article.in.cart.dto.ts
@@ -3,7 +3,6 @@ export class EditArticleInCartDto {
     articleId: number;
 
     @Validator.IsNotEmpty()
-    @Validator.IsPositive()
     @Validator.IsNumber({
         allowInfinity: false,
         allowNaN: false,
