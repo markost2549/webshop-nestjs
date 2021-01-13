daa1a4e40f23f4b9fb9b28bcfb545b947bae01d5
Marko <markost2549@gmail.com>
2021-01-13 17:34:40 +0100

    Config

--- config/storage.config.ts
+++ config/storage.config.ts
@@ -1,20 +1,20 @@
 export const StorageConfig = {
-    photo: {
-        destination: '../storage/photos/',
-        maxAge: 1000 * 60 * 60 * 7,
-        urlPrefix: '/assets/photos',
-        maxSize: 1024 * 1024 * 3,
-        resize: {
-            thumb: {
-                width: 120,
-                height: 100,
-                directory: 'thumb/'
-            },
-            small: {
-                width: 320,
-                height: 240,
-                directory: 'small/'
-            }
-        }
-    }
+  photo: {
+    destination: './storage/photos/',
+    maxAge: 1000 * 60 * 60 * 7,
+    urlPrefix: '/assets/photos',
+    maxSize: 1024 * 1024 * 3,
+    resize: {
+      thumb: {
+        width: 120,
+        height: 100,
+        directory: 'thumb/',
+      },
+      small: {
+        width: 320,
+        height: 240,
+        directory: 'small/',
+      },
+    },
+  },
 };
